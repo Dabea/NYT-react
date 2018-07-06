5da38f2009909787634ab1095e8246e08a8eae50
Abraham <abeis@msn.com>
2018-07-05 22:20:19 -0500

    added nyt api and displaying on page

---
+++ nyt-scraper/src/components/searchComponet/search.js
@@ -0,0 +1,101 @@
+import React, {Component } from 'react';
+import axios from 'axios';
+import "./search.css";
+
+
+class Search extends Component {
+
+    state = {
+        searchTopic: "test",
+        startYear: "",
+        endYear: "",
+        storyies: []
+    }
+
+    buildQueryURL = () => {
+        let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        queryURL += "?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
+        queryURL += "&q=" + this.state.searchTopic;
+      
+        var startYear = this.state.startYear;
+      
+        if (parseInt(startYear)) {
+          queryURL += "&begin_date=" + startYear + "0101";
+        }
+
+        var endYear = this.state.endYear
+      
+        if (parseInt(endYear)) {
+          queryURL += "&end_date=" + endYear + "0101";
+        }
+        axios.get(queryURL).then(res => {
+            console.log(res.data.response.docs)
+            this.setState({
+                storyies: res.data.response.docs
+            });
+        })
+        return queryURL;
+      };
+
+    
+      
+    updateInputValue = (evt)  => {
+        this.setState({
+            searchTopic: evt.target.value
+        });
+    };
+
+    updateInputStartYear = (evt)  => {
+        this.setState({
+            startYear: evt.target.value
+        });
+    };
+
+    updateInputEndYear = (evt)  => {
+        this.setState({
+            endYear: evt.target.value
+        });
+    };
+     
+
+    render() {
+        const Mystyle = {
+            "border": "1px solid black",
+            "margin": "15px"
+          }
+       
+        return (
+            <div>
+                <div  className="panel" >
+                    <div className="panel-header" >
+                        <span> Search </span>
+                    </div>    
+                    <div>
+                        <label> Topic </label>
+                        <input value={this.state.searchTopic} onChange={(event) =>this.updateInputValue(event)} />
+                    </div>
+                    <div>
+                        <label> start Year </label>
+                        <input value={this.state.startYear} onChange={(event) =>this.updateInputStartYear(event)} />
+                    </div>
+                    <div>
+                        <label> end Year </label>
+                        <input value={this.state.endYear} onChange={(event) =>this.updateInputEndYear(event)} />
+                    </div>    
+                    <button onClick={this.buildQueryURL}> Click me</button>
+                   
+                </div>
+                <div className="panel" >
+                    <div className="panel-header" >
+                        <span> Results </span>
+                    </div>    
+                {this.state.storyies.map((story, i) => <div onClick={()=> alert(this.state.storyies[i].headline.kicker) } style={Mystyle} key={'story-' + i}  >{story.headline.main} <button>Save </button> </div>)}
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Search
